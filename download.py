881e97958ac3534578bed67c32080155c1c3ec99
Tan Chia Qian <66521590+TCQian@users.noreply.github.com>
2025-03-21 08:06:41 +0800

    add slurm script

---
+++ download.py
@@ -0,0 +1,6 @@
+import os
+os.environ["HF_HUB_DOWNLOAD_THREADS"] = "1"
+from huggingface_hub import snapshot_download
+target_dir = "./stable-diffusion"
+model_path = snapshot_download(repo_id="stabilityai/stable-diffusion-2-1-base",local_dir=target_dir,)
+print(f"Model downloaded to: {model_path}")
