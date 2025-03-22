bada6ff99478331c61737a6e8b09a1c60ebdfe29
tanchiaqian <66521590+TCQian@users.noreply.github.com>
2025-03-22 21:55:01 +0800

    combine operations

--- sbatch.sh
+++ sbatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=dreambooth            # Job name
+#SBATCH --job-name=dreambooth_original   # Job name
 #SBATCH --time=48:00:00                  # Time limit hrs:min:sec
 #SBATCH --gres=gpu:a100-40:1
 #SBATCH --mail-type=ALL                  # Get email for all status updates
@@ -51,6 +51,40 @@ accelerate launch attacks/aspl.py \
 # ------------------------- Train DreamBooth on perturbed examples -------------------------
 export INSTANCE_DIR="$OUTPUT_DIR/noise-ckpt/50"
 export DREAMBOOTH_OUTPUT_DIR="outputs/$EXPERIMENT_NAME/n000050_DREAMBOOTH"
+
+accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_PATH  \
+  --enable_xformers_memory_efficient_attention \
+  --train_text_encoder \
+  --instance_data_dir=$INSTANCE_DIR \
+  --class_data_dir=$CLASS_DIR \
+  --output_dir=$DREAMBOOTH_OUTPUT_DIR \
+  --with_prior_preservation \
+  --prior_loss_weight=1.0 \
+  --instance_prompt="a photo of sks person" \
+  --class_prompt="a photo of person" \
+  --inference_prompt="a photo of sks person;a dslr portrait of sks person" \
+  --resolution=512 \
+  --train_batch_size=2 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-7 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=200 \
+  --max_train_steps=1000 \
+  --checkpointing_steps=500 \
+  --center_crop \
+  --mixed_precision=bf16 \
+  --prior_generation_precision=bf16 \
+  --sample_batch_size=8
+
+  # ------------------------- Compress instance images -------------------------
+python jpeg_compress.py $OUTPUT_DIR/noise-ckpt/50 --quality 75 --output_dir $OUTPUT_DIR/noise-ckpt/50_compressed
+
+
+# ------------------------- Train DreamBooth on perturbed examples -------------------------
+export INSTANCE_DIR="$OUTPUT_DIR/noise-ckpt/50_compressed"
+export DREAMBOOTH_OUTPUT_DIR="outputs/$EXPERIMENT_NAME/n000050_DREAMBOOTH_compressed"
 
 accelerate launch train_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_PATH  \
