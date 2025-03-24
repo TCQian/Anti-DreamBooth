368a8ab201216060387ddb076d7f35b12c4446ea
tanchiaqian <66521590+TCQian@users.noreply.github.com>
2025-03-24 21:52:43 +0800

    fix script

--- sbatch_adaptive_torch_self.sh
+++ sbatch_adaptive_torch_self.sh
@@ -118,10 +118,10 @@ conda activate dreambooth-evaluate
 
 
 python evaluations/ism_fdfr.py \
-    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch_self/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
 
 python evaluations/ism_fdfr.py \
-    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch_self/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
