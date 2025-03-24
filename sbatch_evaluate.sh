9d5db917178bbe1bfd4655d5580d8ae9d9fdae84
tanchiaqian <66521590+TCQian@users.noreply.github.com>
2025-03-24 21:54:34 +0800

    update evaluation script

--- sbatch_evaluate.sh
+++ sbatch_evaluate.sh
@@ -10,26 +10,42 @@
 source ~/.bashrc
 conda activate dreambooth-evaluate
 
+echo "ISM: Running evaluation for ASPL Normal"
 python evaluations/ism_fdfr.py \
     --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
+echo "ISM: Running evaluation for ASPL Normal Compressed"
 python evaluations/ism_fdfr.py \
     --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
+echo "ISM: Running evaluation for ASPL Adaptive"
 python evaluations/ism_fdfr.py \
     --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
+echo "ISM: Running evaluation for ASPL Adaptive Compressed"
 python evaluations/ism_fdfr.py \
     --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
+echo "ISM: Running evaluation for ASPL Adaptive Torch"
 python evaluations/ism_fdfr.py \
     --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
+echo "ISM: Running evaluation for ASPL Adaptive Torch Compressed"
 python evaluations/ism_fdfr.py \
     --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
+
+echo "ISM: Running evaluation for ASPL Adaptive Torch Self"
+python evaluations/ism_fdfr.py \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch_self/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
+
+echo "ISM: Running evaluation for ASPL Adaptive Torch Self Compressed"
+python evaluations/ism_fdfr.py \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch_self/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
