bc6793bfed3f7dee3b6a4bf5708d1917824ae063
tanchiaqian <66521590+TCQian@users.noreply.github.com>
2025-03-29 10:48:25 +0800

    update evaluation script

--- sbatch_evaluate.sh
+++ sbatch_evaluate.sh
@@ -10,42 +10,139 @@
 source ~/.bashrc
 conda activate dreambooth-evaluate
 
-echo "ISM: Running evaluation for ASPL Normal"
+# ASPL
+echo "ISM: Running evaluation for ASPL"
 python evaluations/ism_fdfr.py \
     --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
-echo "ISM: Running evaluation for ASPL Normal Compressed"
+echo "Brisque: Running evaluation for ASPL"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+echo "ISM: Running evaluation for ASPL Compressed"
 python evaluations/ism_fdfr.py \
     --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
-echo "ISM: Running evaluation for ASPL Adaptive"
+echo "Brisque: Running evaluation for ASPL Compressed"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+
+echo "ISM: Running evaluation for ASPL DCT PGD"
 python evaluations/ism_fdfr.py \
-    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_pgd/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
-echo "ISM: Running evaluation for ASPL Adaptive Compressed"
+echo "Brisque: Running evaluation for ASPL DCT PGD"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_pgd/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+echo "ISM: Running evaluation for ASPL DCT PGD Compressed"
 python evaluations/ism_fdfr.py \
-    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_pgd/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
-echo "ISM: Running evaluation for ASPL Adaptive Torch"
+echo "Brisque: Running evaluation for ASPL DCT PGD Compressed"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_pgd/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+
+echo "ISM: Running evaluation for ASPL DCT Mask"
 python evaluations/ism_fdfr.py \
-    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_mask/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
-echo "ISM: Running evaluation for ASPL Adaptive Torch Compressed"
+echo "Brisque: Running evaluation for ASPL DCT Mask"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_mask/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+echo "ISM: Running evaluation for ASPL DCT Mask Compressed"
 python evaluations/ism_fdfr.py \
-    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_mask/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
-echo "ISM: Running evaluation for ASPL Adaptive Torch Self"
+echo "Brisque: Running evaluation for ASPL DCT Mask Compressed"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_mask/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+
+echo "ISM: Running evaluation for ASPL DCT Gradient"
 python evaluations/ism_fdfr.py \
-    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch_self/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_gradient/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
 
-echo "ISM: Running evaluation for ASPL Adaptive Torch Self Compressed"
+echo "Brisque: Running evaluation for ASPL DCT Gradient"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_gradient/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+echo "ISM: Running evaluation for ASPL DCT Gradient Compressed"
 python evaluations/ism_fdfr.py \
-    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_adaptive_torch_self/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_gradient/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
     --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
+
+echo "Brisque: Running evaluation for ASPL DCT Gradient Compressed"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/ASPL_dct_gradient/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+
+# FSMG
+echo "ISM: Running evaluation for FSMG"
+python evaluations/ism_fdfr.py \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
+
+echo "Brisque: Running evaluation for FSMG"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+echo "ISM: Running evaluation for FSMG Compressed"
+python evaluations/ism_fdfr.py \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
+
+echo "Brisque: Running evaluation for FSMG Compressed"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+
+echo "ISM: Running evaluation for FSMG DCT PGD"
+python evaluations/ism_fdfr.py \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_pgd/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
+
+echo "Brisque: Running evaluation for FSMG DCT PGD"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_pgd/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+echo "ISM: Running evaluation for FSMG DCT PGD Compressed"
+python evaluations/ism_fdfr.py \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_pgd/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
+
+echo "Brisque: Running evaluation for FSMG DCT PGD Compressed"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_pgd/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+
+echo "ISM: Running evaluation for FSMG DCT Mask"
+python evaluations/ism_fdfr.py \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_mask/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
+
+echo "Brisque: Running evaluation for FSMG DCT Mask"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_mask/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+echo "ISM: Running evaluation for FSMG DCT Mask Compressed"
+python evaluations/ism_fdfr.py \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_mask/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
+
+echo "Brisque: Running evaluation for FSMG DCT Mask Compressed"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_mask/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+
+echo "ISM: Running evaluation for FSMG DCT Gradient"
+python evaluations/ism_fdfr.py \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_gradient/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
+
+echo "Brisque: Running evaluation for FSMG DCT Gradient"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_gradient/n000050_DREAMBOOTH/checkpoint-1000/dreambooth/a_photo_of_sks_person
+
+echo "ISM: Running evaluation for FSMG DCT Gradient Compressed"
+python evaluations/ism_fdfr.py \
+    --data_dir /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_gradient/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person \
+    --emb_dirs /home/e/e0407638/github/Anti-DreamBooth/data/n000050/set_B
+
+echo "Brisque: Running evaluation for FSMG DCT Gradient Compressed"
+python evaluations/brisques.py --prompt_path /home/e/e0407638/github/Anti-DreamBooth/outputs/FSMG_dct_gradient/n000050_DREAMBOOTH_compressed/checkpoint-1000/dreambooth/a_photo_of_sks_person
